1af16cba0979eeeb923d245ac016db0872518c95
Jegan R <jeganr@syncfusion.com>
2022-02-17 17:12:07 +0530

    Add demo
    Add demo

---
+++ CustomFiltering.cs
@@ -0,0 +1,59 @@
+﻿using Syncfusion.UI.Xaml.Core;
+using Syncfusion.UI.Xaml.Editors;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GoogleSearchDemo
+{
+    public class CustomFiltering : NotificationObject, IAutoCompleteFilterBehavior
+    {
+        public CustomFiltering()
+        {
+            GetGoogleSuggestions("test");
+        }
+
+        private Task<object> activeTask;
+
+        public Task<object> GetMatchingItemsAsync(SfAutoComplete source, AutoCompleteFilterInfo filterInfo)
+        {
+            activeTask = GetGoogleSuggestions(filterInfo.Text);
+            return activeTask;
+        }
+
+        private async Task<object> GetGoogleSuggestions(string query)
+        {
+            if (string.IsNullOrEmpty(query) || string.IsNullOrWhiteSpace(query))
+            {
+                return new List<string>();
+            }
+
+            string xmlSuggestions;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var searchQuery = String.Format("https://www.google.com/complete/search?output=toolbar&q={0}", query);
+                    xmlSuggestions = await client.GetStringAsync(searchQuery);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            XDocument doc = XDocument.Parse(xmlSuggestions);
+            var suggestions = doc.Descendants("CompleteSuggestion")
+                                    .Select(
+                                        item => item.Element("suggestion").Attribute("data").Value);
+
+            return suggestions.ToList();
+        }
+    }
+}
